23e458adc66682ffd968ba41f47c7933ad4872d8
kartikeya <kartikeya.22b0151031@abes.ac.in>
2025-04-28 09:57:10 +0530

    atlas

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const mongoose =require('mongoose');
 async function dbConn(){
-    const conn=await mongoose.connect('mongodb://localhost:27017/studentapp');
+    const conn=await mongoose.connect('mongodb+srv://kartikeya:root@cluster0.p7eqkxc.mongodb.net/user_registration_app?retryWrites=true&w=majority&appName=Cluster0');
     if(conn){
         console.log('database connect successfully')
 
